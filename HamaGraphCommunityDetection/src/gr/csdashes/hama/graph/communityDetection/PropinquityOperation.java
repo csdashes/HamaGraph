f9691e567051dabda0dd31cdd44d3ecabd3d7a60
nop33 <itrichop@csd.auth.gr>
2013-04-15 02:37:06 +0300

    Create PropinquityOperation enum

---
+++ HamaGraphCommunityDetection/src/gr/csdashes/hama/graph/communityDetection/PropinquityOperation.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.csdashes.hama.graph.communityDetection;
+
+/**
+ *
+ * @author Ilias Trichopoulos <itrichop@csd.auth.gr>
+ */
+public enum PropinquityOperation {
+    INCREASE,DECREASE;
+}

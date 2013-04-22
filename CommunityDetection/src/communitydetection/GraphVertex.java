99d37555e4725240b22ff105448aa303dfdc21f0
nop33 <itrichop@csd.auth.gr>
2013-04-22 18:17:04 +0300

    Print list of neighboors

--- CommunityDetection/src/communitydetection/GraphVertex.java
+++ CommunityDetection/src/communitydetection/GraphVertex.java
@@ -6,8 +6,12 @@ package communitydetection;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -20,19 +24,28 @@ import org.apache.hama.graph.Vertex;
  */
 public class GraphVertex extends Vertex<Text, NullWritable, Text>{
 
+    Set<Text> Nr = new HashSet<Text>(); // The remaining neighboors
+    Set<Text> Ni = new HashSet<Text>(); // The neighboors to be insterted
+    Set<Text> Nd = new HashSet<Text>(); // The neighboors to be deleted
+    // The propinquity value map
+    Map<HashMap<Text,Text>,LongWritable> P = new HashMap<HashMap<Text,Text>,LongWritable>();
+    
+    
     @Override
     public void compute(Iterator<Text> messages) throws IOException {
         List<Edge<Text, NullWritable>> neighboors;
         neighboors = new ArrayList<Edge<Text, NullWritable>>();
         neighboors = this.getEdges();
-        String s = null;
-        int i = 0;
+//        String s = null;
+//        int i = 0;
 //        Text firstNeighboor = neighboors.get(0).getDestinationVertexID();
         for (Edge<Text, NullWritable> edge : neighboors) {
-            s = s + "," + edge.getDestinationVertexID().toString();
-            i++;
+//            s = s + "," + edge.getDestinationVertexID().toString();
+//            i++;
+            Nr.add(edge.getDestinationVertexID());
         }
-        this.setValue(new Text(s));
+//        this.setValue(new Text(s));
+        this.setValue(new Text(Nr.toString()));
         voteToHalt();
     }
     
